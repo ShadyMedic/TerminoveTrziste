cc7a32b5bce4a5c170b1f0ee5835f9d5baa95a91
Jan Štěch <jan.stech@posteo.net>
2024-06-24 19:14:54 +0200

    Implement loading exam date info from SIS and replication per exam subjects

--- Models/Advert.php
+++ Models/Advert.php
@@ -2,6 +2,9 @@
 
 namespace TerminoveTrziste\Models;
 
+use DateTime;
+use DOMDocument;
+use DOMXPath;
 use TerminoveTrziste\Models\Database\Db;
 
 /**
@@ -22,6 +25,10 @@ class Advert
      * @var string Name of the subject (loaded from SIS by its code)
      */
     private string $subject;
+    /**
+     * @var int SIS ID of the offered subject date
+     */
+    private int $offerSisId;
     /**
      * @var string Offered exam date (YYYY-MM-DD format)
      */
@@ -56,17 +63,24 @@ class Advert
      */
     public function generate()
     {
-        $this->id = Db::executeQuery('INSERT INTO advert(`token`) VALUES (?)', [bin2hex(random_bytes(16))], true);
+        $this->token = bin2hex(random_bytes(16));
+        $this->id = Db::executeQuery('INSERT INTO advert(`token`) VALUES (?)', [$this->token], true);
     }
 
     /**
-     * Method loading $subjectCode, $subject and $offer attributes from a link of the webpage in SIS with offered exam
+     * Method loading $subjectCode, $subject, $offerSisId and $offer attributes from a link of the webpage in SIS with offered exam
      * date details
      * @param string $sisLink URL leading to the SIS webpage with this exam date's details
-     * @return bool True if the URL was parsed successfully and data loaded, FALSE otherwise
+     * @return array|false Numerical array of subjects that this exam date was created for (containing associative arrays
+     *  with keys "Name" and "Code", if everything was set up correctly, FALSE otherwise
      */
     public function loadFromSis(string $sisLink): bool
     {
+        //Make sure the link leads to the English version of SIS
+        if (!str_contains($sisLink, 'is.cuni.cz/studium/eng/')) {
+            $sisLink = str_replace('is.cuni.cz/studium/', 'is.cuni.cz/studium/eng/', $sisLink);
+        }
+
         $sisId = $this->extractSisId($sisLink);
         if ($sisId === false) {
             return false;
@@ -75,19 +89,93 @@ class Advert
     }
 
     /**
-     * Method loading $subjectCode, $subject and $offer attributes from SIS ID of the offered exam date
+     * Method loading $subjectCode, $subject, $offerSisId and $offer attributes from SIS ID of the offered exam date
      * @param int $examDateSisId SIS ID of the offered exam date
-     * @return bool TRUE if the data could be loaded (downloaded), FALSE otherwise
+     * @return array|false Numerical array of subjects that this exam date was created for (containing associative arrays
+     * with keys "Name" and "Code", if everything was set up correctly, FALSE otherwise
      */
-    private function loadFromSisId(int $examDateSisId): bool
+    private function loadFromSisId(int $examDateSisId): array|false
     {
-        // TODO
+        $html = file_get_contents($examDateSisId);
+
+        if ($html === false) {
+            return false;
+        }
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML($html);
+        $xpath = new DOMXPath($dom);
+
+        //Load general exam date info (we're interested in date and time)
+        $examDateDetails = array();
+        $select = $xpath->query('//div[@class="form_div"]/table[@class="tab1"]');
+        if ($select->count() === 0) {
+            return false;
+        }
+        $table = $select->item(0);
+        if (is_null($table)) {
+            return false;
+        }
+        $rows = $table->getElementsByTagName('tr');
+        if ($rows->count() === 0) {
+            return false;
+        }
+        foreach ($rows as $row) {
+            $cells = $row->getElementsByTagName('td');
+            $key = trim(strip_tags($cells->item(0)->nodeValue), " \n\r\t\v\0:");
+            $value = strip_tags($cells->item(1)->nodeValue);
+            $data[$key] = $value;
+            /* The table selected by DOMXPath looks like this:
+                <table class="tab1">
+                    <tbody>
+                        <tr><th>Faculty :</th><td><b>Second Faculty of Medicine</b></td></tr>
+                        <tr><th>Guarantor :</th><td><b>Department of Pathology and Molecular Medicine (13-321)</b></td></tr>
+                        <tr><th>Date :</th><td><b>Jun 12, 2024 - Wednesday</b></td></tr>
+                        <tr><th>Time :</th><td><b>08:00</b></td></tr>
+                        <tr><th>End :</th><td><b>Jun 12, 2024, 12:00 AM</b></td></tr>
+                        <tr><th>Registration from :</th><td><b>Apr 11, 2024, 07:00 PM</b></td></tr>
+                        <tr><th>Cancel before :</th><td><b>Jun 11, 2024, 12:00 PM</b></td></tr>
+                        <tr><th>Registration until :</th><td><b>Jun 11, 2024, 12:00 PM</b></td></tr>
+                        <tr><th>Capacity :</th><td><b>10</b></td></tr>
+                        <tr><th>Number of registered :</th><td><b>5</b></td></tr>
+                        <tr><th>Note :</th><td><b>Pouze 1. termíny / Regular 1st examination terms only.</b></td></tr>
+                        <tr><th>Information :</th><td>...</td></tr>
+                    </tbody>
+                </table>
+            */
+        }
+
+        //Load information about subjects this exam date is made for (we're interested in date and time)
+        $subjectsDetails = array();
+        $select = $xpath->query('//div[@id="content"]/table[@class="tab1"]')->item(0);
+        $rows = $xpath->query('.//tr[@class="row1" or @class="row2"]', $select);
+        if ($rows->count() === 0) {
+            return false;
+        }
+        foreach ($rows as $row) {
+            $cells = $row->getElementsByTagName('td');
+            $code = trim(strip_tags($cells->item(0)->nodeValue));
+            $name = trim(strip_tags($cells->item(2)->nodeValue));
+            $subjectsDetails[] = array('Code' => $code, 'Name' => $name);
+
+            /* The rows selected by DOMXPath looks like this:
+            <tr class="row1"><td><a href="..." class="link3">D0105439</a></td><td>FM2</td><td><a href="..." class="link3">Pathology</a></td><td>2023/2024</td><td>summer</td><td>Ex</td></tr>
+            <tr class="row2"><td><a href="..." class="link3">D1105439</a></td><td>FM2</td><td><a href="..." class="link3">Pathology</a></td><td>2023/2024</td><td>summer</td><td>Ex</td></tr>
+            */
+        }
+
+        $this->offerSisId = $examDateSisId;
+        $this->offer = DateTime::createFromFormat('M j, Y;l H:i', $examDateDetails['Date'] . ';' . $examDateDetails['Time'])->format("Y-m-d H:i");
+
+        Db::executeQuery("UPDATE advert SET offer_sis_id = ? AND offer = ? WHERE id = ?;", [$this->offerSisId, $this->offer]);
+
+        return $subjectsDetails;
     }
 
     /**
      * Method extracting SIS ID of the exam date from URL of its details webpage
      * @param string $sisLink URL address of an exam date details webpage (example:
-     * https://is.cuni.cz/studium/term_st2/index.php?id=1c25fb2e557776ed106&tid=&do=zapsane&sub=detail&ztid=813811 )
+     * https://is.cuni.cz/studium/eng/term_st2/index.php?id=1c25fb2e557776ed106&tid=&do=zapsane&sub=detail&ztid=813811 )
      * @return int|bool SIS ID if it could be extracted (813811 from the example above), FALSE otherwise
      */
     private function extractSisId(string $sisLink): int|bool
@@ -109,9 +197,43 @@ class Advert
     }
 
     /**
+     * Method replicating the current instance for every subject the offered exam date was created for,
+     * keeping the following attributes:
+     * - $offerSisId
+     * - $offer
+     * - $token
+     * - $email
+     * - $active
+     * - $highlight
+     * @param array $subjects Array of wanted subject records (
+     *  [['Name'=>'Subject','Code'=>'D000001],['Name'=>'Subject','Code'=>'D000002],['Name'=>'Subject','Code'=>'D000003])
+     * @return array Array of all the advert instances bound to the current operation ($this included)
+     */
+    public function replicateForSubjects(array $subjects): array
+    {
+        $firstSubject = array_shift($subjects);
+        $this->subject = $firstSubject["Name"];
+        $this->subjectCode = $firstSubject["Code"];
+        $instances = [$this];
+
+        // TODO
+        foreach ($subjects as $subject) {
+            $clone = clone $this;
+            $clone->id = Db::executeQuery(
+                'INSERT INTO advert(subject_code, subject, offer_sis_id, offer, token, email, active, highlight) VALUES (?,?,?,?,?,?,?,?);',
+                [$subject["Code"], $subject["Name"], $this->offerSisId, $this->offer, $this->token, $this->email, $this->active, $this->highlight],
+                true
+            );
+        }
+
+        return $instances;
+    }
+
+    /**
      * Method replicating the current instance for every wanted exam date, keeping the following attributes:
      * - $subjectCode
      * - $subject
+     * - $offerSisId
      * - $offer
      * - $token
      * - $email
@@ -129,13 +251,18 @@ class Advert
         foreach ($dates as $date) {
             $clone = clone $this;
             $clone->id = Db::executeQuery(
-                'INSERT INTO advert(subject_code, subject, offer, search, token, email, active, highlight) VALUES (?,?,?,?,?,?,?,?);',
-                [$this->subjectCode, $this->subject, $this->offer, $date, $this->token, $this->email, $this->active, $this->highlight],
+                'INSERT INTO advert(subject_code, subject, offer_sis_id, offer, search, token, email, active, highlight) VALUES (?,?,?,?,?,?,?,?,?);',
+                [$this->subjectCode, $this->subject, $this->offerSisId, $this->offer, $date, $this->token, $this->email, $this->active, $this->highlight],
                 true
             );
         }
 
         return $instances;
+    }
+
+    public function getSisLink()
+    {
+        return 'https://is.cuni.cz/studium/term_st2/index.php?do=zapsat&sub=detail&ztid=' . $this->offerSisId;
     }
 
     /**
@@ -175,36 +302,35 @@ class Advert
      * @param string $message Message written by the person interested in this advert
      * @return bool
      */
-    public function answer(string $replyTo, string $message) : bool
+    public function answer(string $replyTo, string $message): bool
     {
         $headers = [
-            'From' => 'Termínové Tržiště <no-reply@̈́'.$_SERVER['SERVER_NAME'].'>',
+            'From' => 'Termínové Tržiště <no-reply@̈́' . $_SERVER['SERVER_NAME'] . '>',
             'Content-Type' => 'text/plain; charset=UTF-8'
         ];
 
         $body =
-            "Hi there. Somebody replied to your advert placed on ".$_SERVER['SERVER_NAME']."\n".
-            "Their message goes below:\n".
-            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n".
-            wordwrap($message, 70, "\n").
-            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n".
-            "You can get in touch with the person by sending him an e-mail to:\n".
-            "$replyTo\n".
-            "\n".
-            "Please note that your advert has automatically been deactivated\n".
-            "and will be deleted after a few days. If you don't come to an agreement\n".
-            "with the person who reacted, you can reactivate your advert by clicking\n".
-            "the link below. Please do not reactivate your advert if you exchanged\n".
-            "your exam date\n".
-            "\n".
-            "Reactivate the advert:\n".
-            "https://".$_SERVER['SERVER_NAME']."/advert.php?token=".$this->token."\n".
-            "\n".
-            "Best of luck with your exams!\n".
-            "\n".
-            "\n".
-            "This e-mail has been automatically generated."
-        ;
+            "Hi there. Somebody replied to your advert placed on " . $_SERVER['SERVER_NAME'] . "\n" .
+            "Their message goes below:\n" .
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" .
+            wordwrap($message, 70, "\n") .
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" .
+            "You can get in touch with the person by sending him an e-mail to:\n" .
+            "$replyTo\n" .
+            "\n" .
+            "Please note that your advert has automatically been deactivated\n" .
+            "and will be deleted after a few days. If you don't come to an agreement\n" .
+            "with the person who reacted, you can reactivate your advert by clicking\n" .
+            "the link below. Please do not reactivate your advert if you exchanged\n" .
+            "your exam date\n" .
+            "\n" .
+            "Reactivate the advert (or delete it instantly):\n" .
+            "https://" . $_SERVER['SERVER_NAME'] . "/advert.php?token=" . $this->token . "\n" .
+            "\n" .
+            "Best of luck with your exams!\n" .
+            "\n" .
+            "\n" .
+            "This e-mail has been automatically generated.";
 
         $result = mail($this->email, 'Somebody is interested in exchanging your exam date for theirs!', $body, implode("\r\n", $headers));
         if ($result) {
